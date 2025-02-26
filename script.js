c060c467535daff791e70546115253a33a061103
towebl <135875519+towebl@users.noreply.github.com>
2025-02-26 23:44:30 +0000

    Update script.js
    Updated JS file

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.add('active');
+    navbar.classList.toggle('active');
 }
+
 
 document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn => {
     btn.onclick = () => {
